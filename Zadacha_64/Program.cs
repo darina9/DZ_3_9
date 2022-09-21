7e30921fe920282771683c2e0b4ae6f55fca1ffd
darina9 <krastinadv@gmail.com>
2022-09-21 11:11:25 +0300

    Zadacha_64

---
+++ Zadacha_64/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+string NatNum (int number1,int number2)
+{
+    if(number1<=number2) return $"{number1} "+ NatNum(number1+1,number2);
+    else return string.Empty;
+}
+Console.WriteLine(NatNum(M,N));
