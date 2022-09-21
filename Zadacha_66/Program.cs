aaf390f8bdc3860aca257041a6fa3aee99fd4b9c
darina9 <krastinadv@gmail.com>
2022-09-21 11:13:25 +0300

    Zadacha_66

---
+++ Zadacha_66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumMN(int M, int N)
+{
+    int sum = M;
+    if (M == N)
+        return 0;
+    else
+    {
+        M++;
+        sum = M + SumMN(M, N);
+        return sum;
+    }   
+}
+ Console.WriteLine("Сумма чисел " + SumMN(M - 1, N));
