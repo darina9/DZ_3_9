5edf71c02d335d0a8ab532ec8fb58b9ec885c1ed
darina9 <krastinadv@gmail.com>
2022-09-21 11:14:33 +0300

    Zadacha_68

---
+++ Zadacha_68/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman(int M, int N)
+{
+    if (M == 0)
+    {
+        return N + 1;
+    }
+    else if (N == 0 && M != 0)
+    {
+        return Akkerman(M - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(M - 1, Akkerman(M, N - 1)));
+    }        
+}
+ Console.WriteLine($"Функция Аккермана для чисел ({M},{N}) = {Akkerman(M, N)}"); 
